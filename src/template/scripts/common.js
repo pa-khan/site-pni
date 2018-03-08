1b1fabb00bcf2cd308b69be72bca13c04778946e
e5ash <io@e5ash.com>
2018-03-08 17:38:25 +0300

    .

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,32 @@ $(document).ready(function($) {
 
 	$('.text_phone').mask('+7 (999) 999-99-99');
 	
+
+
+	var eventsList = $('.main-events__list'),
+			eventsNav = $('.main-events__nav'),
+			galleryCortege = $('.gallery__cortege'),
+			logos = $('.logos__list');
+
+	eventsList.slick({
+		fade: true,
+		asNavFor: eventsNav,
+		arrows: false
+	});
+
+	eventsNav.slick({
+		slidesToShow: 3,
+		asNavFor: eventsList,
+		focusOnSelect: true
+	});
+
+	galleryCortege.slick({
+		arrows: false,
+		dots: true
+	})
+
+	logos.slick({
+		slidesToShow: 5
+	})
+
 });
